488cf3abc46f197ef59c53263806b0d93acf2541
apollo <apollo@DESKTOP-UPD06S5>
2022-03-01 14:25:12 -0400

    Code cleanup

--- main.js
+++ main.js
@@ -9,10 +9,19 @@
         update the map to get the new bus positions
     */ 
 
-    // https://hrmbusapi.herokuapp.com/
-    // https://github.com/bbecquet/Leaflet.RotatedMarker
-    // https://leafletjs.com/SlavaUkraini/reference.html#marker
-    // https://leafletjs.com/SlavaUkraini/reference.html#geojson
+    var busAPIData = null;
+    const geojsonArray = [];
+
+    // Create an icon for the bus
+    var busIcon = L.Icon.extend ({
+        options: {
+            iconUrl: 'bus.png',
+            iconSize: [40,60],
+            iconAnchor: [20, 30]
+        }
+    });
+
+    var yellowBus = new busIcon();
 
     // API Call
     fetch('https://hrmbusapi.herokuapp.com/')
@@ -20,14 +29,11 @@
     .then((json) => {
 
         // Hold the raw data from the HRM API
-        const busAPIData = json.entity;
+        busAPIData = json.entity;
         
         // Filter the raw data for all busses on routes 1-10 (variations include 9A, 9B, 6C, 7A, 7B)
         const rawRoutesOneToTen = busAPIData
         .filter(bus => (bus.vehicle.trip.routeId.replace(/[a-zA-Z]/g, "")) <= 10);
-        
-        // Store the geojson formated features
-        const geojsonArray = [];
 
         // Create the geoJSON features
         rawRoutesOneToTen.forEach(bus => {
@@ -67,56 +73,14 @@
             .addTo(map);
         });
 
-        console.log(geojsonArray);
-
-        /*
-            Marker accepts coordinants as latlng - geoJSON accpets coordinants as lnglat
-
-            Create an empty geoJSON layer
-                var myLayer = L.geoJSON().addTo(map)
-            Add to it as geoJSON features are created from the API data
-                myLayer.addData(geojsonFeature);
-            
-            rawRoutesOneToTen.forEach(bus => {
-                bus.vehicle.position.latitude, bus.vehicle.position.longitude], 
-                {
-                    icon: yellowBus,
-                    rotationAngle: bus.vehicle.position.bearing,
-                    rotationOrigin: 'center center'
-                })
-                .addTo(map)
-                .bindPopup(bus.vehicle.trip.routeId);
-            });
-
-        */
-                
+        console.log(geojsonArray);     
     });
-
-    var busIcon = L.Icon.extend ({
-        options: {
-            iconUrl: 'bus.png',
-            iconSize: [40,60],
-            iconAnchor: [20, 30]
-        }
-    });
-
-    var yellowBus = new busIcon();
-    var bearing = 315;
 
     let map = L.map('theMap').setView([44.650627, -63.597140], 14);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-    // Demo bus
-    L.marker([44.6702995300293, -63.57426071166992], 
-        {
-            icon: yellowBus, 
-            rotationAngle: bearing, 
-            rotationOrigin: 'center center'
         })
         .addTo(map);
-
 
 })()
